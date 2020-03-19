3a50cd7a962facd8858eefdc9e7c1a58cc685b8e
Valdecir Fernandes da Silva Filho <valdecir.filho@gmail.com>
2020-03-19 14:00:07 -0300

    Create index.js
    commit do arquivo inicial de scraping

---
+++ index.js
@@ -0,0 +1,9 @@
+const scrapeIt = (require("scrape-it);
+
+scrapeIt("https://www.infomoney.com.br/cotacoes/viavarejo-vvar3/", {
+  title: "h1"
+  , value: "div.value p
+  , percentage: "div.percentage p
+}).then(page =>
+  console.log(page)
+).catch( err => console.log(err));
