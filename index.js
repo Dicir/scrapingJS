9939c43bb3c6f264bf68faa5018bfc74f5c11662
Valdecir Fernandes <dicir@dicir.com.br>
2022-02-15 20:58:17 -0300

    Corrigindo erro de requeri, identação e formatação do código

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const scrapeIt = (require("scrape-it);
+import scrapeIt from "scrape-it";
 
 scrapeIt("https://www.infomoney.com.br/cotacoes/viavarejo-vvar3/", {
-  title: "h1"
-  , value: "div.value p
-  , percentage: "div.percentage p
+    title: "h1",
+    value: "div.value p",
+    percentage: "div.percentage p"
 }).then(page =>
-  console.log(page)
-).catch( err => console.log(err));
+    console.log(page)
+).catch(err => console.log(err));
